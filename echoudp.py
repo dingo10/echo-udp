6ff69676ab4ba461a1027a5922fc199061b74eba
Pranav Pandit <p.pandit@f5.com>
2018-03-16 17:15:19 -0700

    New File py

---
+++ echoudp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import socket
+import os
+
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = int(os.environ['UDPPORT'])
+
+print "Listening on UDP port",UDP_PORT
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print data
+    Message = "Data Received on" UDP_PORT
+    sock.sendto(Message, (addr))
+
